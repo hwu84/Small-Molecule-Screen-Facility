bd682a1f0bdcb9bfbf10dcf8bcdcf7a880a86b70
hwu84 <wuhaozhen@hotmail.com>
2016-03-01 14:53:26 -0600

    Added files via upload

---
+++ FingerPrint.java
@@ -0,0 +1,15 @@
+
+import org.apache.lucene.util.OpenBitSet;
+
+
+
+public class FingerPrint {
+    String moleculeName;
+    OpenBitSet fp;
+    long card;
+    int idx;
+    int[] bitfreqs;
+    int hashcode;
+    int[] bytebitfreqs;
+
+}
